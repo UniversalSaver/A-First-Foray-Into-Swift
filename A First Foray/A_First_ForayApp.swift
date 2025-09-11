664072a83babbf734556491cf7d89a0530a0a152
nabhanrashid <nabhandb06@gmail.com>
2025-09-11 12:12:02 -0400

    Initial Commit

---
+++ A First Foray/A_First_ForayApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_First_ForayApp.swift
+//  A First Foray
+//
+//  Created by Nabhan Rashid on 2025-09-11.
+//
+
+import SwiftUI
+
+@main
+struct A_First_ForayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
